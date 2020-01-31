15b1ef6a7a4d0f1abe09cac8a35c1ee84f92a7ef
Gareth Gomersall <gareth.gomersall@justgiving.com>
2020-01-31 16:04:19 +0000

    adds netlify headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac2c8990f19179397e58ed12d52639c5e041e92eb5b8a20751c1aa18437c8b23
-size 1294
+oid sha256:20930be8c762762b98e51063852f5b01f20599c23d7aceee7dd6c78b4cf7ddaa
+size 1324
