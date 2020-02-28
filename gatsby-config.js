14cc12d3ffd3633a2a236a11c4a385238be15b3f
Gareth Gomersall <gareth.gomersall@justgiving.com>
2020-02-28 09:29:31 +0000

    added netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20930be8c762762b98e51063852f5b01f20599c23d7aceee7dd6c78b4cf7ddaa
-size 1324
+oid sha256:c010a0373b1150626a502caba57ac4ad42736c8c24248b4f3164066886049bfd
+size 1357
