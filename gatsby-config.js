ee948c7c1e666c5b5d7c84e16052725a1e16a9ad
Gareth Gomersall <gareth.gomersall@justgiving.com>
2020-02-28 10:13:42 +0000

    added sitemaps

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c010a0373b1150626a502caba57ac4ad42736c8c24248b4f3164066886049bfd
-size 1357
+oid sha256:74ae746297887cf09bb3bbe8f6343b88fdb65c612dba999ed22db6864899600f
+size 1330
