1f17377114fc7fed4b3a17ed5da44ab7a4da363c
Gareth Gomersall <gareth.gomersall@justgiving.com>
2020-03-13 15:46:01 +0000

    added custom 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
+import { useStaticQuery, graphql } from 'gatsby';
+
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-);
+const NotFoundPage = () => {
+  const data = useStaticQuery(graphql`
+    query FourOhFourQuery {
+      file(relativePath: { regex: "/error/" }) {
+        publicURL
+      }
+    }
+  `);
+
+  return (
+    <Layout>
+      <SEO title="404: Not found" />
+      <h1>NOT FOUND</h1>
+      <img src={data.file.publicURL} alt="JustGiving 404 Robo" width="400" />
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Layout>
+  );
+};
 
 export default NotFoundPage;
