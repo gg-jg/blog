dc972fce9122196878aa9b69896d5bacbf5cdaab
Gareth Gomersall <gareth.gomersall@justgiving.com>
2020-03-27 08:42:18 +0000

    added tags with styling and time to read

--- src/components/listing.js
+++ src/components/listing.js
@@ -25,6 +25,16 @@ const Listing = () => {
       text-decoration: underline;
       color: #524763;
     }
+    .tag-wrapper span {
+      background-color: purple;
+      margin-right: 10px;
+      margin-top: 10px;
+      padding: 5px 15px;
+      border-radius: 500px;
+      color: white;
+      display: inline-block;
+      font-weight: 500;
+    }
   `;
 
   const data = useStaticQuery(graphql`
@@ -38,10 +48,12 @@ const Listing = () => {
           node {
             id
             excerpt
+            timeToRead
             frontmatter {
               title
               slug
               date(formatString: "DD MMMM, YYYY")
+              tags
             }
             timeToRead
           }
@@ -53,16 +65,27 @@ const Listing = () => {
   return (
     <div>
       {data.allMarkdownRemark.edges.map(
-        ({ node: { frontmatter, excerpt } }) => (
+        ({ node: { frontmatter, excerpt, timeToRead } }) => (
           <Post key={frontmatter.slug}>
             <Link to={`/posts${frontmatter.slug}`}>
               <h2>{frontmatter.title}</h2>
             </Link>
-            <p>{frontmatter.date}</p>
+            <div style={{ display: 'flex', fontWeight: 500 }}>
+              <p>{frontmatter.date}</p>
+              <p style={{ margin: '0 10px' }}> - </p>
+              <p>{timeToRead} min read</p>
+            </div>
             <p>{excerpt}</p>
             <Link to={`/posts${frontmatter.slug}`} className="read-more">
               Read More
             </Link>
+            {frontmatter.tags && (
+              <div className="tag-wrapper">
+                {frontmatter.tags.map(tag => {
+                  return <span>{tag}</span>;
+                })}
+              </div>
+            )}
           </Post>
         )
       )}
